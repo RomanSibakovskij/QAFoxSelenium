fe64d38ebec94bfbb967fe91c0460f90cdf0b85f
RomanSibakovskij <j4023958@gmail.com>
2025-02-25 16:37:56 +0200

    Add empty RegisterPage sheet

---
+++ src/main/java/com/tutorialsninja/qafox/RegisterPage.java
@@ -0,0 +1,14 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+
+public class RegisterPage extends BasePage{
+
+
+    public RegisterPage(WebDriver driver) {super(driver);}
+
+    
+}
