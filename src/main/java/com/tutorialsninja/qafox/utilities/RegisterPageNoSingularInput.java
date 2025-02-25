5ada138d246c527db85eb923e573159ed904abc8
RomanSibakovskij <j4023958@gmail.com>
2025-02-25 18:58:15 +0200

    Add relevant web elements for RegisterPageNoSingularInput utility sheet

--- src/main/java/com/tutorialsninja/qafox/utilities/RegisterPageNoSingularInput.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/RegisterPageNoSingularInput.java
@@ -2,8 +2,23 @@ package com.tutorialsninja.qafox.utilities;
 
 import com.tutorialsninja.qafox.utilities.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
 
 public class RegisterPageNoSingularInput extends BasePage{
+
+    @FindBy(xpath = "//fieldset[@id='account']//input[@id='input-firstname']")
+    private WebElement registerPageFirstNameInputField;
+    @FindBy(xpath = "//fieldset[@id='account']//input[@id='input-lastname']")
+    private WebElement registerPageLastNameInputField;
+    @FindBy(xpath = "//fieldset[@id='account']//input[@id='input-email']")
+    private WebElement registerPageEmailInputField;
+    @FindBy(xpath = "//fieldset[@id='account']//input[@id='input-telephone']")
+    private WebElement registerPagePhoneInputField;
+    //password section
+    @FindBy(xpath = "//fieldset[2]//input[@id='input-password']")
+    private WebElement registerPagePasswordInputField;
+    @FindBy(xpath = "//fieldset[2]//input[@id='input-confirm']")
+    private WebElement registerPageConfirmPasswordInputField;
 
 
     public RegisterPageNoSingularInput(WebDriver driver) {super(driver);}
