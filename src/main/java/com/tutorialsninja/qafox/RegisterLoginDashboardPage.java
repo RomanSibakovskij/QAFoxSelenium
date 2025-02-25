134ff26b0709f026e2b56a47164d0420ca44f4e2
RomanSibakovskij <j4023958@gmail.com>
2025-02-25 16:31:32 +0200

    Add empty RegisterLoginDashboardPage sheet

---
+++ src/main/java/com/tutorialsninja/qafox/RegisterLoginDashboardPage.java
@@ -0,0 +1,16 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.*;
+import com.tutorialsninja.qafox.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+
+public class RegisterLoginDashboardPage extends BasePage{
+
+
+
+    public RegisterLoginDashboardPage(WebDriver driver) {super(driver);}
+
+    
+}
