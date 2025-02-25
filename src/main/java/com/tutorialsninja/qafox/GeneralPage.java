81c180d43601741c90b180f2f0559f1698227f2a
RomanSibakovskij <j4023958@gmail.com>
2025-02-25 16:09:49 +0200

    Add empty GeneralPage sheet (for elements the majority of pages of the app share)

---
+++ src/main/java/com/tutorialsninja/qafox/GeneralPage.java
@@ -0,0 +1,13 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class GeneralPage extends BasePage {
+
+    
+
+    public GeneralPage(WebDriver driver) {super(driver);}
+
+
+}
