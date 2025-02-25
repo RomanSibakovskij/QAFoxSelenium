21c9699a7b75600a308a0c7a4be868c7a36ee5cb
RomanSibakovskij <j4023958@gmail.com>
2025-02-25 16:52:43 +0200

    Add empty TestDataGenerator utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/TestDataGenerator.java
@@ -0,0 +1,14 @@
+package com.tutorialsninja.qafox.utilities;
+
+import org.openqa.selenium.*;
+import java.security.SecureRandom;
+import java.util.*;
+
+public class TestDataGenerator extends BasePage{
+
+
+    public TestDataGenerator(WebDriver driver) {super(driver);}
+
+
+    
+}
