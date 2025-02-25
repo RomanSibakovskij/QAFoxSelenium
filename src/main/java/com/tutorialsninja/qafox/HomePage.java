1f6f9cbb23284d2f57bb44a2c90eaeef805917ba
RomanSibakovskij <j4023958@gmail.com>
2025-02-25 16:18:59 +0200

    Add empty HomePage sheet

---
+++ src/main/java/com/tutorialsninja/qafox/HomePage.java
@@ -0,0 +1,15 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+import java.util.*;
+
+public class HomePage extends BasePage {
+
+
+    public HomePage(WebDriver driver) {super(driver);}
+
+    
+}
