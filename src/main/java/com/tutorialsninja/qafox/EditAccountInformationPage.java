046cd6befbf60b1accb89d402e1bbdea98503acd
RomanSibakovskij <j4023958@gmail.com>
2025-02-26 08:26:42 +0200

    Refactor EditAccountPage to EditAccountInfoPage sheet

---
+++ src/main/java/com/tutorialsninja/qafox/EditAccountInformationPage.java
@@ -0,0 +1,12 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+
+public class EditAccountInformationPage extends BasePage{
+
+
+    public EditAccountInformationPage(WebDriver driver) {super(driver);}
+
+
+}
