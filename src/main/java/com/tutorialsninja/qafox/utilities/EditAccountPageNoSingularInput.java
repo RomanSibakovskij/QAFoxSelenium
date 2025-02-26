06d49ad673d0f11d591b108408e282ab8914f161
RomanSibakovskij <j4023958@gmail.com>
2025-02-26 15:19:55 +0200

    Add relevant web elements for EditAccountPageNoSingularInput

--- src/main/java/com/tutorialsninja/qafox/utilities/EditAccountPageNoSingularInput.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/EditAccountPageNoSingularInput.java
@@ -6,8 +6,17 @@ import org.openqa.selenium.support.*;
 
 public class EditAccountPageNoSingularInput extends BasePage{
 
+    //input form
+    @FindBy(xpath = "//input[@id='input-firstname']")
+    private WebElement editAccountInfoFirstNameInputField;
+    @FindBy(xpath = "//input[@id='input-lastname']")
+    private WebElement editAccountInfoLastNameInputField;
+    @FindBy(xpath = "//input[@id='input-email']")
+    private WebElement editAccountInfoEmailInputField;
+    @FindBy(xpath = "//input[@id='input-telephone']")
+    private WebElement editAccountInfoPhoneInputField;
 
     public EditAccountPageNoSingularInput(WebDriver driver) {super(driver);}
 
-    
+
 }
