d3406ff104707f9b0af033e2192c62ba3ed9b9db
RomanSibakovskij <j4023958@gmail.com>
2025-02-26 06:41:32 +0200

    Add empty RegisterPageTooLongSingularInput utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/RegisterPageTooLongSingularInput.java
@@ -0,0 +1,13 @@
+package com.tutorialsninja.qafox.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterPageTooLongSingularInput extends BasePage{
+
+
+
+    public RegisterPageTooLongSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
