9ccc87a192d76614eb545b6c0d3638feaa81e4a0
RomanSibakovskij <j4023958@gmail.com>
2025-02-26 07:36:57 +0200

    Add empty RegisterPageInvalidSingularInputFormat utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/RegisterPageInvalidSingularInputFormat.java
@@ -0,0 +1,12 @@
+package com.tutorialsninja.qafox.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterPageInvalidSingularInputFormat extends BasePage{
+
+
+    public RegisterPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
+
+    
+}
