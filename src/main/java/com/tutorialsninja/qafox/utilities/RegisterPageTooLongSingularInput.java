43689157e0e83b46d9053fac2387e0cea1604850
RomanSibakovskij <j4023958@gmail.com>
2025-02-26 06:42:30 +0200

    Add relevant web elements for RegisterPageTooLongSingularInput

--- src/main/java/com/tutorialsninja/qafox/utilities/RegisterPageTooLongSingularInput.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/RegisterPageTooLongSingularInput.java
@@ -5,9 +5,24 @@ import org.openqa.selenium.support.FindBy;
 
 public class RegisterPageTooLongSingularInput extends BasePage{
 
-
+    @FindBy(xpath = "//fieldset[@id='account']//input[@id='input-firstname']")
+    private WebElement registerPageFirstNameInputField;
+    @FindBy(xpath = "//fieldset[@id='account']//input[@id='input-lastname']")
+    private WebElement registerPageLastNameInputField;
+    @FindBy(xpath = "//fieldset[@id='account']//input[@id='input-email']")
+    private WebElement registerPageEmailInputField;
+    @FindBy(xpath = "//fieldset[@id='account']//input[@id='input-telephone']")
+    private WebElement registerPagePhoneInputField;
+    //password section
+    @FindBy(xpath = "//fieldset[2]//input[@id='input-password']")
+    private WebElement registerPagePasswordInputField;
+    @FindBy(xpath = "//fieldset[2]//input[@id='input-confirm']")
+    private WebElement registerPageConfirmPasswordInputField;
+    //invalid singular input message
+    @FindBy(xpath = "//div[@class='text-danger']")
+    private WebElement registerPageInvalidSingularInputError;
 
     public RegisterPageTooLongSingularInput(WebDriver driver) {super(driver);}
 
-    
+
 }
