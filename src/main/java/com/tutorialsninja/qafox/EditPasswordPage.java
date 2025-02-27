2bdea8142c13d7a76f78b916e5e5dfeb21e5bb52
RomanSibakovskij <j4023958@gmail.com>
2025-02-27 06:53:25 +0200

    Add empty EditPasswordPage utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/EditPasswordPage.java
@@ -0,0 +1,15 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+
+public class EditPasswordPage extends BasePage{
+
+
+
+    public EditPasswordPage(WebDriver driver) {super(driver);}
+
+    
+}
