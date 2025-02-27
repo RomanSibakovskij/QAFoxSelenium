dc08e72bd3c1b3f287cb99c768d7829b2233e8ca
RomanSibakovskij <j4023958@gmail.com>
2025-02-27 05:36:12 +0200

    Add relevant web elements for EditAccountPageTooLongSingularInput

--- src/main/java/com/tutorialsninja/qafox/utilities/EditAccountPageTooLongSingularInput.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/EditAccountPageTooLongSingularInput.java
@@ -5,9 +5,20 @@ import org.openqa.selenium.support.*;
 
 public class EditAccountPageTooLongSingularInput extends BasePage{
 
-
+    //input form
+    @FindBy(xpath = "//input[@id='input-firstname']")
+    private WebElement editAccountInfoFirstNameInputField;
+    @FindBy(xpath = "//input[@id='input-lastname']")
+    private WebElement editAccountInfoLastNameInputField;
+    @FindBy(xpath = "//input[@id='input-email']")
+    private WebElement editAccountInfoEmailInputField;
+    @FindBy(xpath = "//input[@id='input-telephone']")
+    private WebElement editAccountInfoPhoneInputField;
+    //invalid singular input message element
+    @FindBy(xpath = "//div[@class='text-danger']")
+    private WebElement editAccountInfoInvalidSingularInputError;
 
     public EditAccountPageTooLongSingularInput(WebDriver driver) {super(driver);}
 
-    
+
 }
