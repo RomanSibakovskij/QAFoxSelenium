8fee779f0f0286f2fcfe1fc418e4d4c2c8678d84
RomanSibakovskij <j4023958@gmail.com>
2025-02-27 05:35:11 +0200

    Add empty EditAccountPageTooLongSingularInput utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/EditAccountPageTooLongSingularInput.java
@@ -0,0 +1,13 @@
+package com.tutorialsninja.qafox.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class EditAccountPageTooLongSingularInput extends BasePage{
+
+
+
+    public EditAccountPageTooLongSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
