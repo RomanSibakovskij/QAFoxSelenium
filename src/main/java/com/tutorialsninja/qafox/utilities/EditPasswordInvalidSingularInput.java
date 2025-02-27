9c8488788a105db133d41ca748e251c1e4b9462f
RomanSibakovskij <j4023958@gmail.com>
2025-02-27 07:12:01 +0200

    Add empty EditPasswordInvalidSingularInput utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/EditPasswordInvalidSingularInput.java
@@ -0,0 +1,12 @@
+package com.tutorialsninja.qafox.utilities;
+
+import com.tutorialsninja.qafox.utilities.BasePage;
+import org.openqa.selenium.*;
+
+public class EditPasswordInvalidSingularInput extends BasePage{
+
+
+    public EditPasswordInvalidSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
