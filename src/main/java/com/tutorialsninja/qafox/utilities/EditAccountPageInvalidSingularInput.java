592a955d8db193f25ed07f821ee66795f0bd915e
RomanSibakovskij <j4023958@gmail.com>
2025-02-27 06:06:36 +0200

    Add empty EditAccountPageInvalidSingularInput utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/EditAccountPageInvalidSingularInput.java
@@ -0,0 +1,13 @@
+package com.tutorialsninja.qafox.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class EditAccountPageInvalidSingularInput extends BasePage{
+
+
+
+    public EditAccountPageInvalidSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
