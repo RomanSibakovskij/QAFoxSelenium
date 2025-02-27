5295f505f77bc91139e9dfc74c80ddaf7b47ee94
RomanSibakovskij <j4023958@gmail.com>
2025-02-27 07:13:09 +0200

    Add relevant web elements for EditPasswordInvalidSingularInput

--- src/main/java/com/tutorialsninja/qafox/utilities/EditPasswordInvalidSingularInput.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/EditPasswordInvalidSingularInput.java
@@ -2,11 +2,17 @@ package com.tutorialsninja.qafox.utilities;
 
 import com.tutorialsninja.qafox.utilities.BasePage;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
 
 public class EditPasswordInvalidSingularInput extends BasePage{
 
+    //input form
+    @FindBy(xpath = "//input[@id='input-password']")
+    private WebElement passwordInputField;
+    @FindBy(xpath = "//input[@id='input-confirm']")
+    private WebElement confirmPasswordInputField;
 
     public EditPasswordInvalidSingularInput(WebDriver driver) {super(driver);}
 
-    
+
 }
