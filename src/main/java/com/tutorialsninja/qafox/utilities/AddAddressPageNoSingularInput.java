fe459f1a67e666a88a6651432edc4d4f72711710
RomanSibakovskij <j4023958@gmail.com>
2025-02-27 08:23:43 +0200

    Add empty AddAddressPageNoSingularInput utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/AddAddressPageNoSingularInput.java
@@ -0,0 +1,17 @@
+package com.tutorialsninja.qafox.utilities;
+
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class AddAddressPageNoSingularInput extends BasePage{
+
+
+    public AddAddressPageNoSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
