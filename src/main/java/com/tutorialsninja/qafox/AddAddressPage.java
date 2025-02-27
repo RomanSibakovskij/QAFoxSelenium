3a231144e7610f9dd6780dea28be43d999fe937d
RomanSibakovskij <j4023958@gmail.com>
2025-02-27 07:59:13 +0200

    Add empty AddAddressPage sheet

---
+++ src/main/java/com/tutorialsninja/qafox/AddAddressPage.java
@@ -0,0 +1,17 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class AddAddressPage extends BasePage{
+
+
+    public AddAddressPage(WebDriver driver) {super(driver);}
+
+    
+}
