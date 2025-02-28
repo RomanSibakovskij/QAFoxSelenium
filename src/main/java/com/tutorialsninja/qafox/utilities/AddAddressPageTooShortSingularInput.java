3bee6c26aef1c77af56303b8b82498fecff5533c
RomanSibakovskij <j4023958@gmail.com>
2025-02-28 05:04:10 +0200

    Add empty AddAddressPageTooShortSingularInput utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/AddAddressPageTooShortSingularInput.java
@@ -0,0 +1,19 @@
+package com.tutorialsninja.qafox.utilities;
+
+import com.tutorialsninja.qafox.RegisterPage;
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class AddAddressPageTooShortSingularInput extends BasePage{
+
+
+
+    public AddAddressPageTooShortSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
