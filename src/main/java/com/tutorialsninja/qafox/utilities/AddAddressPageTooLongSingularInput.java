ab29aee01ae652f2c1cb48de62c924873347059b
RomanSibakovskij <j4023958@gmail.com>
2025-02-28 05:44:47 +0200

    Add empty AddAddressPageTooLongSingularInput utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/AddAddressPageTooLongSingularInput.java
@@ -0,0 +1,15 @@
+package com.tutorialsninja.qafox.utilities;
+
+import com.tutorialsninja.qafox.RegisterPage;
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class AddAddressPageTooLongSingularInput extends BasePage{
+
+
+
+    public AddAddressPageTooLongSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
