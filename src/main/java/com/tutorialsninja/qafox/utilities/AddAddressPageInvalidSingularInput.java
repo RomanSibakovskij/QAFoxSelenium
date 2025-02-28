3715bdc84948600109bfbef8d3782a35780203fa
RomanSibakovskij <j4023958@gmail.com>
2025-02-28 06:24:57 +0200

    Add relevant web elements for AddAddressPageInvalidSingularInput

--- src/main/java/com/tutorialsninja/qafox/utilities/AddAddressPageInvalidSingularInput.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/AddAddressPageInvalidSingularInput.java
@@ -7,9 +7,22 @@ import org.openqa.selenium.support.*;
 
 public class AddAddressPageInvalidSingularInput extends BasePage{
 
-
+    //add address page input form web elements (required)
+    @FindBy(xpath = "//input[@id='input-firstname']")
+    private WebElement addAddressFirstNameInputField;
+    @FindBy(xpath = "//input[@id='input-lastname']")
+    private WebElement addAddressLastNameInputField;
+    @FindBy(xpath = "//input[@id='input-address-1']")
+    private WebElement addAddressAddress1InputField;
+    @FindBy(xpath = "//input[@id='input-city']")
+    private WebElement addAddressCityInputField;
+    @FindBy(xpath = "//input[@id='input-postcode']")
+    private WebElement addAddressPostCodeInputField;
+    //invalid singular input message element
+    @FindBy(xpath = "//div[@class='text-danger']")
+    private WebElement addAddressInvalidSingularInputError;
 
     public AddAddressPageInvalidSingularInput(WebDriver driver) {super(driver);}
 
-    
+
 }
