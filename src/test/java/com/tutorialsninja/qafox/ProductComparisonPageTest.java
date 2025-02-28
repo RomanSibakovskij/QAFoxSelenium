2c52f6782dcf62f29408a97c1112c0b04d6dea79
RomanSibakovskij <j4023958@gmail.com>
2025-02-28 16:34:20 +0200

    Add Set Products (HP LP3065, Iphone, Canon EOS 5ED) To Compare Page Test (as a registered user) method

--- src/test/java/com/tutorialsninja/qafox/ProductComparisonPageTest.java
+++ src/test/java/com/tutorialsninja/qafox/ProductComparisonPageTest.java
@@ -9,7 +9,7 @@ public class ProductComparisonPageTest extends TestMethods{
 
     //Test 010 -> add set products (HP LP3065, Iphone, Canon EOS 5ED) to product compare page test (as a guest)
     @Test
-    @DisplayName("Add Set Products (HP LP3065, Iphone, Canon EOS 5ED) To Wishlist Page Test (as a guest)")
+    @DisplayName("Add Set Products (HP LP3065, Iphone, Canon EOS 5ED) To Compare Page Test (as a guest)")
     @Tag("Add_Products_To_Comparison_Page")
     @Tag("Test_As_A_Guest")
     void addProductsToCompareListAsGuestTest() {
@@ -19,5 +19,26 @@ public class ProductComparisonPageTest extends TestMethods{
         addProductsToCompareListTest();
     }
 
+    //test as a registered user
+
+    //Test 010a -> add set products (HP LP3065, Iphone, Canon EOS 5ED) to product compare page test (as a registered user)
+    @Test
+    @DisplayName("Add Set Products (HP LP3065, Iphone, Canon EOS 5ED) To Compare Page Test (as a registered user)")
+    @Tag("Add_Products_To_Comparison_Page")
+    @Tag("Test_As_A_Reg_User")
+    void addProductToCompareListAsRegUserTest() {
+        RegisterPage registerPage = new RegisterPage(driver);
+        AddAddressPage addAddressPage = new AddAddressPage(driver);
+        //user navigation to 'Register' page test
+        userNavigationToRegisterPageTest();
+        //valid user account creation
+        validUserAccountCreationTest(registerPage);
+        //valid user address addition test
+        validUserSingleMainAddressAdditionTest(addAddressPage);
+        //navigate to 'Desktops' category page test (as a registered user)
+        navigateToDesktopsCategoryDashboardPageRegUserTest();
+        //add products (HP LP3065, Iphone, Canon EOS 5ED) to compare list (as a registered user)
+        addProductsToCompareListTest();
+    }
 
 }
