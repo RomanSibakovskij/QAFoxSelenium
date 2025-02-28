fb0e3e55daca95a580e075a98980a21b78044f3a
RomanSibakovskij <j4023958@gmail.com>
2025-02-28 16:00:49 +0200

    Add empty SingleProductCategoryDashboardPage sheet

---
+++ src/main/java/com/tutorialsninja/qafox/SingleProductCategoryDashboardPage.java
@@ -0,0 +1,20 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SingleProductCategoryDashboardPage extends BasePage {
+
+
+    public SingleProductCategoryDashboardPage(WebDriver driver) {super(driver);}
+
+
+    
+}
