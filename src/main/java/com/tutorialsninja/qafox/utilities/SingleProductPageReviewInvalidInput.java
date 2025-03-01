8744f9df0e32e3df3db25fb8be6b3da8a31f9204
RomanSibakovskij <j4023958@gmail.com>
2025-03-01 08:41:03 +0200

    Add relevant web elements for SingleProductPageReviewInvalidInput

--- src/main/java/com/tutorialsninja/qafox/utilities/SingleProductPageReviewInvalidInput.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/SingleProductPageReviewInvalidInput.java
@@ -5,6 +5,17 @@ import org.openqa.selenium.support.*;
 
 public class SingleProductPageReviewInvalidInput extends BasePage{
 
+    //reviews section
+    @FindBy(xpath = "//input[@id='input-name']")
+    private WebElement singleProductPageProductReviewUserNameInputField;
+    @FindBy(xpath = "//textarea[@id='input-review']")
+    private WebElement singleProductPageProductReviewInputField;
+    @FindBy(xpath = "//input[@name='rating'][3]")
+    private WebElement singleProductPageProductAvgRatingRadioButton;
+    @FindBy(xpath = "//button[@id='button-review']")
+    private WebElement singleProductPageProductSubmitReviewButton;
+    @FindBy(xpath = "//div[@class='alert alert-danger alert-dismissible']")
+    private WebElement singleProductPageProductReviewFailMessage;
 
     public SingleProductPageReviewInvalidInput(WebDriver driver) {super(driver);}
 
