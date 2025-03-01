3617423a9a0fe7f33bf38a93ad12dbdbc3d638e1
RomanSibakovskij <j4023958@gmail.com>
2025-03-01 06:58:28 +0200

    Add Single Product (HP LP3065) To Cart Test (as a registered user) test method

--- src/test/java/com/tutorialsninja/qafox/SingleProductCategoryDashboardPageTest.java
+++ src/test/java/com/tutorialsninja/qafox/SingleProductCategoryDashboardPageTest.java
@@ -19,4 +19,24 @@ public class SingleProductCategoryDashboardPageTest extends TestMethods{
         addHPLP3065ToCartTest();
     }
 
+    //Test 011a -> single product (HP LP3065) addition to cart page test (as a registered user)
+    @Test
+    @DisplayName("Add Single Product (HP LP3065) To Cart Test (as a registered user)")
+    @Tag("Single_Product_Addition_To_Cart")
+    @Tag("Test_As_A_Reg_User")
+    void singleProductAdditionToCartAsRegUserTest() {
+        RegisterPage registerPage = new RegisterPage(driver);
+        AddAddressPage addAddressPage = new AddAddressPage(driver);
+        //user navigation to 'Register' page test
+        userNavigationToRegisterPageTest();
+        //valid user account creation
+        validUserAccountCreationTest(registerPage);
+        //valid user address addition test
+        validUserSingleMainAddressAdditionTest(addAddressPage);
+        //navigate to 'Desktops' category page test (as a registered user)
+        navigateToDesktopsCategoryDashboardPageRegUserTest();
+        //single product (HP LP3065) addition to cart page test (as a registered user)
+        addHPLP3065ToCartTest();
+    }
+
 }
