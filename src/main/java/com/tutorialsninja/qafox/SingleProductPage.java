1b8011d31130d99b16cfef2ac8ba0600c8a124cb
RomanSibakovskij <j4023958@gmail.com>
2025-03-01 05:37:00 +0200

    Add empty SingleProductPage sheet

---
+++ src/main/java/com/tutorialsninja/qafox/SingleProductPage.java
@@ -0,0 +1,18 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+
+public class SingleProductPage extends BasePage{
+
+
+    public SingleProductPage(WebDriver driver) {super(driver);}
+
+    
+}
