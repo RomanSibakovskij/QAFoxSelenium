c19de29f717b0f6c23e084ad2521dddb97c6380f
RomanSibakovskij <j4023958@gmail.com>
2025-03-02 05:23:58 +0200

    Add Multiple Product (HP LP3065) To Checkout Test (as a registered user) method

--- src/test/java/com/tutorialsninja/qafox/ShoppingCartPageTest.java
+++ src/test/java/com/tutorialsninja/qafox/ShoppingCartPageTest.java
@@ -47,15 +47,37 @@ public class ShoppingCartPageTest extends TestMethods{
 
     //Test 015b -> multiple products (HP LP3065) addition to check out page test (as a guest)
     @Test
-    @DisplayName("Add Multiple Product (HP LP3065) To Checkout Test (as a guest)")
+    @DisplayName("Add Multiple Products (HP LP3065) To Checkout Test (as a guest)")
     @Tag("Multiple_Products_Addition_To_Checkout")
     @Tag("Test_As_A_Guest")
-    void multipleProductAdditionToCheckoutAsGuestTest() {
+    void multipleProductsAdditionToCheckoutAsGuestTest() {
         //navigate to 'Desktops' category page test (as a guest)
         navigateToDesktopsCategoryDashboardPageTest();
         //multiple products (HP LP3065) addition to cart page test (as a guest)
         addMultipleHPLP3065ProductsToCartGuestTest();
         //multiple products (HP LP3065) addition to check out page test (as a guest)
         addHPLP3065ProductToCheckoutGuestTest();
+    }
+
+    //Test 015c -> multiple products (HP LP3065) addition to check out page test (as a registered user)
+    @Test
+    @DisplayName("Add Multiple Products (HP LP3065) To Checkout Test (as a registered user)")
+    @Tag("Multiple_Products_Addition_To_Checkout")
+    @Tag("Test_As_A_Reg_User")
+    void multipleProductsAdditionToCheckoutAsRegUserTest() {
+        RegisterPage registerPage = new RegisterPage(driver);
+        AddAddressPage addAddressPage = new AddAddressPage(driver);
+        //user navigation to 'Register' page test
+        userNavigationToRegisterPageTest();
+        //valid user account creation
+        validUserAccountCreationTest(registerPage);
+        //valid user address addition test
+        validUserSingleMainAddressAdditionTest(addAddressPage);
+        //navigate to 'Desktops' category page test (as a registered user)
+        navigateToDesktopsCategoryDashboardPageRegUserTest();
+        //multiple products (HP LP3065) addition to cart page test (as a registered user)
+        addMultipleHPLP3065ProductsToCartRegUserTest();
+        //multiple products (HP LP3065) addition to check out page test (as a registered user)
+        addHPLP3065ProductToCheckoutRegUserTest();
     }
 }
