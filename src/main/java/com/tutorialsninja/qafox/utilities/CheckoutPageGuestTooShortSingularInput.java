ef7a7975ec178a3058f3ca57302aacd62bcfd973
RomanSibakovskij <j4023958@gmail.com>
2025-03-02 14:54:33 +0200

    Add empty CheckoutPageGuestTooShortSingularInput utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageGuestTooShortSingularInput.java
@@ -0,0 +1,16 @@
+package com.tutorialsninja.qafox.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class CheckoutPageGuestTooShortSingularInput extends BasePage{
+
+
+    public CheckoutPageGuestTooShortSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
