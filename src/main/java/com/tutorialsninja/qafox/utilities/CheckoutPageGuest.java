419766a09654982c59ba0cfb36183db94c0072e2
RomanSibakovskij <j4023958@gmail.com>
2025-03-02 06:35:27 +0200

    Add empty CheckoutPageGuest utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageGuest.java
@@ -0,0 +1,18 @@
+package com.tutorialsninja.qafox.utilities;
+
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+
+public class CheckoutPageGuest extends BasePage{
+
+
+    public CheckoutPageGuest(WebDriver driver) {super(driver);}
+
+    
+}
