961425253cc7d7e012895934efef05ad2e16d64b
RomanSibakovskij <j4023958@gmail.com>
2025-03-02 05:19:08 +0200

    Add Multiple Product (HP LP3065) To Checkout Test (as a guest) method

--- src/test/java/com/tutorialsninja/qafox/ShoppingCartPageTest.java
+++ src/test/java/com/tutorialsninja/qafox/ShoppingCartPageTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.*;
 
 public class ShoppingCartPageTest extends TestMethods{
 
+    //single product addition to check out tests
+
     //Test 015 -> single product (HP LP3065) addition to check out page test (as a guest)
     @Test
     @DisplayName("Add Single Product (HP LP3065) To Checkout Test (as a guest)")
@@ -39,5 +41,21 @@ public class ShoppingCartPageTest extends TestMethods{
         addHPLP3065ToCartTest();
         //single product (HP LP3065) addition to check out page test (as a registered user)
         addHPLP3065ProductToCheckoutRegUserTest();
+    }
+
+    //multiple product addition to check out tests
+
+    //Test 015b -> multiple products (HP LP3065) addition to check out page test (as a guest)
+    @Test
+    @DisplayName("Add Multiple Product (HP LP3065) To Checkout Test (as a guest)")
+    @Tag("Multiple_Products_Addition_To_Checkout")
+    @Tag("Test_As_A_Guest")
+    void multipleProductAdditionToCheckoutAsGuestTest() {
+        //navigate to 'Desktops' category page test (as a guest)
+        navigateToDesktopsCategoryDashboardPageTest();
+        //multiple products (HP LP3065) addition to cart page test (as a guest)
+        addMultipleHPLP3065ProductsToCartGuestTest();
+        //multiple products (HP LP3065) addition to check out page test (as a guest)
+        addHPLP3065ProductToCheckoutGuestTest();
     }
 }
