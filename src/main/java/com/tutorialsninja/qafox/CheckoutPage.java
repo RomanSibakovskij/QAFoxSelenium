5250c4b49d2f416bdb4565e805e9df2e26edd3a4
RomanSibakovskij <j4023958@gmail.com>
2025-03-02 06:26:58 +0200

    Add empty CheckoutPage sheet

---
+++ src/main/java/com/tutorialsninja/qafox/CheckoutPage.java
@@ -0,0 +1,20 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CheckoutPage extends BasePage {
+
+
+
+    public CheckoutPage(WebDriver driver) {super(driver);}
+
+    
+}
