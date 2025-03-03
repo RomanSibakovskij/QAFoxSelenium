c5f231422716aba6eab02567115009d50557c6ec
RomanSibakovskij <j4023958@gmail.com>
2025-03-03 14:12:04 +0200

    Add Single Product (HP LP3065) Checkout Confirmation With Valid Checkout Login Test (as a registered user) test method

--- src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageInvalidRegUserLogin.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageInvalidRegUserLogin.java
@@ -4,6 +4,10 @@ import com.tutorialsninja.qafox.RegisterPage;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CheckoutPageInvalidRegUserLogin extends BasePage{
 
@@ -18,7 +22,40 @@ public class CheckoutPageInvalidRegUserLogin extends BasePage{
     @FindBy(xpath = "//div[@class='alert alert-danger alert-dismissible']")
     private WebElement checkoutPageReturningCustomerInvalidLoginInputError;
 
+    //valid registered user data (for remaining inputs)
+    private String validRegUserLoginEmail;
+    private String validRegUserLoginPassword;
+
+    //no singular input
+    private String noValidRegUserLoginEmail;
+
     public CheckoutPageInvalidRegUserLogin(WebDriver driver) {super(driver);}
 
-    
+    //valid registered user login credentials input methods (for remaining inputs)
+    public void inputCheckoutLoginEmailIntoEmailInputField() {checkoutPageReturningCustomerSectionEmailInputField.sendKeys(validRegUserLoginEmail);}
+    public void inputCheckoutLoginPasswordIntoPasswordInputField() {checkoutPageReturningCustomerSectionPasswordInputField.sendKeys(validRegUserLoginPassword);}
+
+    //valid registered user credentials getter
+    public void invalidRegUserLoginCheckoutDataNoEmailGetter(RegisterPage registerPage) {
+
+        noValidRegUserLoginEmail = "";
+        validRegUserLoginPassword = registerPage.getUserPassword();
+
+        System.out.println("Invalid checkout registered user login credentials (no login email): " + "\n");
+        logger.info("No checkout login email: " + noValidRegUserLoginEmail);
+        logger.info("Valid checkout login password (no login email): " + validRegUserLoginPassword);
+        System.out.println("\n");
+
+    }
+
+    //invalid registered user login credentials input methods - no singular input
+    public void inputNoCheckoutLoginEmailIntoEmailInputField() {checkoutPageReturningCustomerSectionEmailInputField.sendKeys(noValidRegUserLoginEmail);}
+
+
+    //invalid singular input error message getter
+    public String getInvalidSingularInputErrorMessage() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(2100));
+        wait.until(ExpectedConditions.visibilityOf(checkoutPageReturningCustomerInvalidLoginInputError));
+        return checkoutPageReturningCustomerInvalidLoginInputError.getText();
+    }
 }
