892953efd30905445282cd4cc418e5729616ea9f
RomanSibakovskij <j4023958@gmail.com>
2025-03-03 13:53:46 +0200

    Add relevant elements for CheckoutPageInvalidRegUserLogin

--- src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageInvalidRegUserLogin.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageInvalidRegUserLogin.java
@@ -7,9 +7,18 @@ import org.openqa.selenium.support.*;
 
 public class CheckoutPageInvalidRegUserLogin extends BasePage{
 
-    
+    //returning customer section web elements
+    @FindBy(xpath = "//div[@id='collapse-checkout-option']//div[@class='col-sm-6'][2]//input[@id='input-email']")
+    private WebElement checkoutPageReturningCustomerSectionEmailInputField;
+    @FindBy(xpath = "//div[@id='collapse-checkout-option']//div[@class='col-sm-6'][2]//input[@id='input-password']")
+    private WebElement checkoutPageReturningCustomerSectionPasswordInputField;
+    @FindBy(xpath = "//div[@id='collapse-checkout-option']//div[@class='col-sm-6'][2]/input")
+    private WebElement checkoutPageReturningCustomerLoginButton;
+    //invalid singular input error element
+    @FindBy(xpath = "//div[@class='alert alert-danger alert-dismissible']")
+    private WebElement checkoutPageReturningCustomerInvalidLoginInputError;
 
     public CheckoutPageInvalidRegUserLogin(WebDriver driver) {super(driver);}
 
-
+    
 }
