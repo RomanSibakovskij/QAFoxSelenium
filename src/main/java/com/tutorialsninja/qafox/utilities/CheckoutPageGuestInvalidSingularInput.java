d14923d1131defa17d752dea2d063fd4bcfbf2f6
RomanSibakovskij <j4023958@gmail.com>
2025-03-03 06:19:56 +0200

    Add empty CheckoutPageGuestInvalidSingularInput utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageGuestInvalidSingularInput.java
@@ -0,0 +1,15 @@
+package com.tutorialsninja.qafox.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class CheckoutPageGuestInvalidSingularInput extends BasePage{
+    
+
+    public CheckoutPageGuestInvalidSingularInput(WebDriver driver) {super(driver);}
+
+
+}
