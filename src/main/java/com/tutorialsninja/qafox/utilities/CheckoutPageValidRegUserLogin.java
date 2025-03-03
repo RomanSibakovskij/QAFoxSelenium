08de18e47a69cffb905cc92c28133522357ca9c2
RomanSibakovskij <j4023958@gmail.com>
2025-03-03 13:13:44 +0200

    Add empty CheckoutPageValidRegUserLogin utility sheet

---
+++ src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageValidRegUserLogin.java
@@ -0,0 +1,16 @@
+package com.tutorialsninja.qafox.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class CheckoutPageValidRegUserLogin extends BasePage{
+
+
+    public CheckoutPageValidRegUserLogin(WebDriver driver) {super(driver);}
+
+    
+}
