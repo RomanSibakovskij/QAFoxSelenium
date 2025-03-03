b04aec4e4c71d69ebc2e205f9dc00bd4ded9f64a
RomanSibakovskij <j4023958@gmail.com>
2025-03-03 06:20:57 +0200

    Add relevant web elements for CheckoutPageGuestInvalidSingularInput

--- src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageGuestInvalidSingularInput.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageGuestInvalidSingularInput.java
@@ -7,7 +7,25 @@ import org.openqa.selenium.support.ui.*;
 import java.time.Duration;
 
 public class CheckoutPageGuestInvalidSingularInput extends BasePage{
-    
+
+    //billing details section
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][1]//input[@id='input-payment-firstname']")
+    private WebElement checkoutPageFirstNameInputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][1]//input[@id='input-payment-lastname']")
+    private WebElement checkoutPageLastNameInputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][1]//input[@id='input-payment-email']")
+    private WebElement checkoutPageEmailInputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][1]//input[@id='input-payment-telephone']")
+    private WebElement checkoutPagePhoneInputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][2]//input[@id='input-payment-address-1']")
+    private WebElement checkoutPageAddress1InputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][2]//input[@id='input-payment-city']")
+    private WebElement checkoutPageCityInputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][2]//input[@id='input-payment-postcode']")
+    private WebElement checkoutPagePostCodeInputField;
+    //invalid singular input error message element
+    @FindBy(xpath = "//div[@class='text-danger']")
+    private WebElement checkoutPageInvalidGuestInputError;
 
     public CheckoutPageGuestInvalidSingularInput(WebDriver driver) {super(driver);}
 
