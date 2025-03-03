2978baf05b875a798bbb7cea51a3e4f9c912dbb6
RomanSibakovskij <j4023958@gmail.com>
2025-03-03 05:09:57 +0200

    Add relevant web elements for CheckoutPageTooLongSingularInput

--- src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageTooLongSingularInput.java
+++ src/main/java/com/tutorialsninja/qafox/utilities/CheckoutPageTooLongSingularInput.java
@@ -8,8 +8,27 @@ import java.time.Duration;
 
 public class CheckoutPageTooLongSingularInput extends BasePage{
 
+    //billing details section
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][1]//input[@id='input-payment-firstname']")
+    private WebElement checkoutPageFirstNameInputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][1]//input[@id='input-payment-lastname']")
+    private WebElement checkoutPageLastNameInputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][1]//input[@id='input-payment-email']")
+    private WebElement checkoutPageEmailInputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][1]//input[@id='input-payment-telephone']")
+    private WebElement checkoutPagePhoneInputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][2]//input[@id='input-payment-address-1']")
+    private WebElement checkoutPageAddress1InputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][2]//input[@id='input-payment-city']")
+    private WebElement checkoutPageCityInputField;
+    @FindBy(xpath = "//div[@id='collapse-payment-address']//div[@class='col-sm-6'][2]//input[@id='input-payment-postcode']")
+    private WebElement checkoutPagePostCodeInputField;
+    //invalid singular input error message element
+    @FindBy(xpath = "//div[@class='text-danger']")
+    private WebElement checkoutPageInvalidGuestInputError;
+
 
     public CheckoutPageTooLongSingularInput(WebDriver driver) {super(driver);}
 
-    
+
 }
