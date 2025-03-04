ed40c6281788b4e3a1e9b7aaac23ba495d861bb3
RomanSibakovskij <j4023958@gmail.com>
2025-03-04 05:27:37 +0200

    Add empty ConfirmOrderSuccessPage sheet

---
+++ src/main/java/com/tutorialsninja/qafox/ConfirmOrderSuccessPage.java
@@ -0,0 +1,19 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+
+
+public class ConfirmOrderSuccessPage extends BasePage {
+
+
+    public ConfirmOrderSuccessPage(WebDriver driver) {super(driver);}
+
+    
+}
