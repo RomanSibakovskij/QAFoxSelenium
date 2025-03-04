dde86e18555b304732cf810c31d092ba3ab312f9
RomanSibakovskij <j4023958@gmail.com>
2025-03-04 05:35:35 +0200

    Add empty OrderHistoryPage sheet

---
+++ src/main/java/com/tutorialsninja/qafox/OrderHistoryPage.java
@@ -0,0 +1,17 @@
+package com.tutorialsninja.qafox;
+
+import com.tutorialsninja.qafox.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class OrderHistoryPage extends BasePage {
+
+
+    public OrderHistoryPage(WebDriver driver) {super(driver);}
+
+    
+}
